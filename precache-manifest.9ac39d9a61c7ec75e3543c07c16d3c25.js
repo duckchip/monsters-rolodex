221cf129de878bc64535dd7ecf7fb296478b9be6
duckchip <tristan.hofman@telenet.Be>
2020-01-15 21:59:19 +0100

    Updates

---
+++ precache-manifest.9ac39d9a61c7ec75e3543c07c16d3c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4880a6b9ba6dbd2367d7c3b616457379",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08ca36c79842141e1e72",
+    "url": "/monsters-rolodex/static/css/main.5363fe65.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08ca36c79842141e1e72",
+    "url": "/monsters-rolodex/static/js/main.e1eb481e.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
